6a4e72f61973f61c8d69946380c08f6b325da4ef
Cameron Messer <cmess77@protonmail.com>
2022-05-02 01:53:38 -0400

    Edit sensor_code to reflect first draft changes.

--- sensor_code/sensor_code.ino
+++ sensor_code/sensor_code.ino
@@ -1,57 +1,45 @@
 /*
 TODO
 
-    IMPLEMENT INPUT FROM RASPBERRY PI
-    TEST/TIME SENSOR CODE
-
 */
 
 #include <Arduino.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-const int SELECTOR_PIN_A =  22;
-const int SELECTOR_PIN_B =  23;
-const int SELECTOR_PIN_C =  24;
-const int STARTING_MUX_PIN =  25;
+// const int SELECTOR_PIN_A =  22;
+// const int SELECTOR_PIN_B =  23;
+// const int SELECTOR_PIN_C =  24;
+// const int STARTING_MUX_PIN =  25;
 
-const int numMux = 8;
-const int numHallPerMux = 8;
-const int numHall = 64;
+// const int numMux = 8;
+// const int numHallPerMux = 8;
+const int num_hall = 4;
 
-const int selectorBinaries[8][3] = {
-    {0, 0, 0},
-    {0, 0, 1},
-    {0, 1, 0},
-    {0, 1, 1},
-    {1, 0, 0},
-    {1, 0, 1},
-    {1, 1, 0},
-    {1, 1, 1}
-};
-
-int *hallList;
-int *muxList;
-
-int testFlag = 1;
-int moveStart[2];
-int moveEnd[2];
-int numChanges = 0;
+int num_changes = 0;
 
 typedef struct Move {
-    int start[2] = {0, 0};
-    int end[2] = {0, 0};
-    char startColumnLetter = 0;
-    char endColumnLetter = 0;
+    char origin[2];
+    char destination[2];
+    int origin_col_num;
+    int destination_col_num;
 }move;
 
-move recentMove;
+move recent_move;
+
+typedef struct Hall {
+    char square[3];
+    int pin_number;
+    int state;
+}hall;
+
+hall *hall_list;
 
 
-int *returnArray(int n) {
+int *return_hall_array(int n) {
     /*
     Description:
-    Function to dynamically allocate memory for an array of size 'n.' 
+    Function to dynamically allocate memory for an array of size n.
     ----------------
     Input:
         int n: number of elements to allocate to array
@@ -60,12 +48,12 @@ int *returnArray(int n) {
         int* temp: pointer to array
     */
 
-   int *temp = new int(n);
+   int *temp = new hall(n);
    return temp;
 }
 
 
-char numToLetter(int num) {
+char letter_to_num(char letter) {
     /*
     Description:
     Function to change a number 0-7 into the corresponding 
@@ -77,31 +65,31 @@ char numToLetter(int num) {
     Output:
         char temp: column in letter form to be used outside of function
     */
-    char temp;
-    switch(num){
-        case 0:
-            temp = 'a';
+    int temp;
+    switch(letter){
+        case 'a':
+            temp = 0;
             break;
-        case 1:
-            temp = 'b';
+        case 'b':
+            temp = 1;
             break;
-        case 2:
-            temp = 'c';
+        case 'c':
+            temp = 2;
             break;
-        case 3:
-            temp = 'd';
+        case 'd':
+            temp = 3;
             break;
-        case 4:
-            temp = 'e';
+        case 'e':
+            temp = 4;
             break;
-        case 5:
-            temp = 'f';
+        case 'f':
+            temp = 5;
             break;
-        case 6:
-            temp = 'g';
+        case 'g':
+            temp = 6;
             break;
-        case 7:
-            temp = 'h';
+        case 'f':
+            temp = 7;
             break;
     }
     return temp;
@@ -120,46 +108,75 @@ void updateHalls() {
     ----------------
     Output:
         Technically nothing.
-        hallList: not returned, but all values are updated via 
+        hall_list: not returned, but all values are updated via 
         reference to hallList
     */
-    for(int i = 0; i < numMux; i++) {
-        for(int j = 0; j < numHallPerMux; j++) {
-            digitalWrite(SELECTOR_PIN_A, selectorBinaries[i][0]);
-            digitalWrite(SELECTOR_PIN_B, selectorBinaries[i][1]);
-            digitalWrite(SELECTOR_PIN_C, selectorBinaries[i][2]);
 
-            // do nothing if the cell hasn't changed
-            if(hallList[j + (i * 8)] == digitalRead(muxList[i]));
+    // for(int i = 0; i < numMux; i++) {
+    //     for(int j = 0; j < numHallPerMux; j++) {
+    //         digitalWrite(SELECTOR_PIN_A, selectorBinaries[i][0]);
+    //         digitalWrite(SELECTOR_PIN_B, selectorBinaries[i][1]);
+    //         digitalWrite(SELECTOR_PIN_C, selectorBinaries[i][2]);
 
-            else {
-                hallList[j + (i * 8)] = digitalRead(muxList[i]);
-                numChanges++;
+    //         // do nothing if the cell hasn't changed
+    //         if(hallList[j + (i * 8)] == digitalRead(muxList[i]));
 
-                switch(numChanges) {
-                    case 1:
-                        recentMove.start[0] = j;
-                        recentMove.start[1] = i;
-                        recentMove.startColumnLetter = numToLetter(recentMove.start[0]);
-                        break;
+    //         else {
+    //             hallList[j + (i * 8)] = digitalRead(muxList[i]);
+    //             numChanges++;
+
+    //             switch(numChanges) {
+    //                 case 1:
+    //                     recentMove.start[0] = j;
+    //                     recentMove.start[1] = i;
+    //                     recentMove.startColumnLetter = numToLetter(recentMove.start[0]);
+    //                     break;
                         
-                    case 2:
-                        recentMove.end[0] = j;
-                        recentMove.end[1] = i;
-                        recentMove.endColumnLetter = numToLetter(recentMove.end[0]);
+    //                 case 2:
+    //                     recentMove.end[0] = j;
+    //                     recentMove.end[1] = i;
+    //                     recentMove.endColumnLetter = numToLetter(recentMove.end[0]);
 
-                        Serial.print(recentMove.startColumnLetter);
-                        Serial.print(" ");
-                        Serial.print(recentMove.start[1] + 1);
-                        Serial.print(" ");
-                        Serial.print(recentMove.endColumnLetter);
-                        Serial.print(" ");
-                        Serial.println(recentMove.end[1] + 1);
-                        delay(1000);
+    //                     Serial.print(recentMove.startColumnLetter);
+    //                     Serial.print(" ");
+    //                     Serial.print(recentMove.start[1] + 1);
+    //                     Serial.print(" ");
+    //                     Serial.print(recentMove.endColumnLetter);
+    //                     Serial.print(" ");
+    //                     Serial.println(recentMove.end[1] + 1);
+    //                     delay(1000);
 
-                        numChanges = 0;
-                        break;
-                }
+    //                     numChanges = 0;
+    //                     break;
+    //             }
+    //         }
+    //     }
+    // }
+    for(int i = 0; i < num_hall; i++) {
+        int temp = digitalRead(hall_list[i].pin_number);
+        if(hall_list[i].state == temp);
+
+        else {
+            hall_list[i].state = temp;
+            num_changes++;
+
+            switch(num_changes) {
+                case 1:
+                    recent_move.origin = hall_list[i].square;
+                    recent_move.origin_col_num = letter_to_num(recent_move.origin[0]);
+                    break;
+                    
+                case 2:
+                    recent_move.destination = hall_list[i].square;
+                    recent_move.destination_col_num = letter_to_num(recent_move.destination_col);
+
+                    Serial.print(recent_move.origin);
+                    Serial.print(" ");
+                    Serial.print(recent_move.destination);
+                    delay(1000);
+
+                    numChanges = 0;
+                    break;
             }
         }
     }
@@ -169,21 +186,20 @@ void updateHalls() {
 void setup() {
     Serial.begin(9600);
 
-    hallList = returnArray(numHall);
-    muxList = returnArray(numMux);
+    hall_list = returnArray(num_hall);
+    int starting_pin = 22;
+    
+    hall_list[0].square = "e 7";
+    hall_list[1].square = "e 5";
+    hall_list[2].square = "e 4";
+    hall_list[3].square = "e 2";
 
-    for(int i = 0; i < numMux; i++) {
-        muxList[i] = STARTING_MUX_PIN + i;
+    for(int i = 0; i < num_hall; i++) {
+        hall_list[i].pin_number = starting_pin + i;
+        pinMode(hall_list[i].pin_number, INPUT_PULLUP);
+
+        hall_list[i].state = digitalRead(hall_list[i].pin_number);
     }
-
-    for(int i = 0; i < numMux; i++) {
-        pinMode(muxList[i], INPUT);
-    }
-
-    pinMode(SELECTOR_PIN_A, OUTPUT);
-    pinMode(SELECTOR_PIN_B, OUTPUT);
-    pinMode(SELECTOR_PIN_C, OUTPUT);
-
 
 }
 
